10f535827307d649a5b81a4e8a8847320207d3cc
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-03-26 09:51:50 -0400

    Added Runtime Permissions + Listing of mp3 files on device

--- app/src/main/java/com/example/nikhil/tempo/MainActivity.java
+++ app/src/main/java/com/example/nikhil/tempo/MainActivity.java
@@ -1,13 +1,127 @@
 package com.example.nikhil.tempo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean permissionsGranted = false;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkAndRequestPermissions();
+        initMp3FilesList();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if(permissionsGranted)
+        {
+            scanDeviceForMp3Files();
+        }
+    }
+
+    public void initMp3FilesList()
+    {
+        if(permissionsGranted)
+        {
+            scanDeviceForMp3Files();
+        }
+    }
+
+    public void checkAndRequestPermissions() {
+        int readExtResult = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int writeExtResult = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        if ((readExtResult != PackageManager.PERMISSION_GRANTED) || (writeExtResult != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+        } else {
+            permissionsGranted = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(getApplicationContext(), "Tempo Permissions are granted!", Toast.LENGTH_SHORT).show();
+                permissionsGranted = true;
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Need to enable Permissions!", Toast.LENGTH_LONG).show();
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    private List<String> scanDeviceForMp3Files()
+    {
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.DURATION
+        };
+        final String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
+        List<String> mp3Files = new ArrayList<>();
+
+        Cursor cursor = null;
+        try {
+            Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            cursor = getContentResolver().query(uri, projection, selection, null, sortOrder);
+            if( cursor != null){
+                cursor.moveToFirst();
+
+                while( !cursor.isAfterLast() ){
+                    String title = cursor.getString(0);
+                    String artist = cursor.getString(1);
+                    String path = cursor.getString(2);
+                    String displayName  = cursor.getString(3);
+                    String songDuration = cursor.getString(4);
+                    cursor.moveToNext();
+                    if(path != null && path.endsWith(".mp3")) {
+                        mp3Files.add(path);
+                    }
+                }
+
+            }
+
+            // print to see list of mp3 files
+            for( String file : mp3Files) {
+                Log.i("TAG", file);
+            }
+
+        } catch (Exception e) {
+            Log.e("TAG", e.toString());
+        }finally{
+            if( cursor != null){
+                cursor.close();
+            }
+        }
+        return mp3Files;
     }
 }
